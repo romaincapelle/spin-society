2e44d1a4c42e7e7a275d1fa60968f4daff6f4038
Romain Capelle <romain.capelle@gmail.com>
2024-04-11 16:20:01 -0700

    Add /contact and homepage in controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,14 @@
 class PagesController < ApplicationController
   def about
+    # no code to describe the view to render the about page because it is the default behavior of Rails to render the view with the same name as the action name
+    # in this case, the view is app/views/pages/about.html.erb
+    # if you want to render a different view, you can use the render method
+    # render 'pages/other'
+  end
+
+  def contact
+  end
+
+  def home
   end
 end
