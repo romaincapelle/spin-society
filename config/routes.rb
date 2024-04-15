ed9919542135b018b3a053599404b91a8425cd64
Romain Capelle <romain.capelle@gmail.com>
2024-04-15 12:25:11 -0700

    Add all 7 CRUD action on clubs

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,21 @@ Rails.application.routes.draw do
   # root "posts#index"
   root to: 'pages#home'
 
+  # PAGES ROUTES
   get "about", to: "pages#about"
   get "contact", to: "pages#contact"
+
+  # CLUBS ROUTES
+#   get "clubs", to: "clubs#index"
+#   get "clubs/new", to: "clubs#new" # #new needs to be before #show otherwise rails will think that "new" is an id
+#   get "clubs/:id", to: "clubs#show", as: :club
+#   post "clubs", to: "clubs#create"
+#   get "clubs/:id/edit", to: "clubs#edit", as: :edit_club
+#   patch "clubs/:id", to: "clubs#update"
+#   delete "clubs/:id", to: "clubs#destroy"
+  # or
+#   resources :clubs
+resources :clubs, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+
+
 end
